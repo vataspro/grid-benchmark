7dec0a839d386978c438d16cfa4c8e202e84b2a2
Alexis Provatas <2414441@swansea.ac.uk>
2025-10-23 00:58:14 +0100

    add Wilson and Kernel Instantiations to SpWilsonD

---
+++ Instantiation/Sp4Fund/WilsonFermion5DInstantiationSpWilsonImplD.cc
@@ -0,0 +1,9 @@
+#include <Grid/qcd/action/fermion/FermionCore.h>
+#include <Grid/qcd/action/fermion/implementation/WilsonFermion5DImplementation.h>
+#include "Implementation.hpp"
+
+NAMESPACE_BEGIN(Grid);
+
+template class WilsonFermion5D<Sp4FundWilsonImplD>; 
+
+NAMESPACE_END(Grid);
